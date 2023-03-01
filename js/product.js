5c8e9f850d49734ab35465db219527b127ef36ab
CuongGosu <hoangngocquoccuong@gmail.com>
2023-03-01 17:52:00 +0700

    update dom

--- js/product.js
+++ js/product.js
@@ -1,25 +1,24 @@
-var productAPI = "http://localhost:3000/product";
-const itemList = document.querySelector(".view-products");
-const gridViewBtn = document.querySelector(".view-grid");
-const detailsViewBtn = document.querySelector(".view-detail");
+var productAPI = 'http://localhost:3000/product';
+const itemList = document.querySelector('.view-products');
+const gridViewBtn = document.querySelector('.view-grid');
+const detailsViewBtn = document.querySelector('.view-detail');
 
-
-gridViewBtn.classList.add("active-btn");
-gridViewBtn.addEventListener("click", () => {
-  gridViewBtn.classList.add("active-btn");
-  detailsViewBtn.classList.remove("active-btn");
-  itemList.classList.remove("info-detail");
+gridViewBtn.classList.add('active-btn');
+gridViewBtn.addEventListener('click', () => {
+  gridViewBtn.classList.add('active-btn');
+  detailsViewBtn.classList.remove('active-btn');
+  itemList.classList.remove('info-detail');
 });
 
-detailsViewBtn.addEventListener("click", () => {
-  detailsViewBtn.classList.add("active-btn");
-  gridViewBtn.classList.remove("active-btn");
-  itemList.classList.add("info-detail");
+detailsViewBtn.addEventListener('click', () => {
+  detailsViewBtn.classList.add('active-btn');
+  gridViewBtn.classList.remove('active-btn');
+  itemList.classList.add('info-detail');
 });
 
-async function getProducts(cb){
+async function getProducts(cb) {
   fetch(productAPI)
-    .then((res)=>{
+    .then((res) => {
       return res.json();
     })
     .then(cb);
@@ -28,15 +27,16 @@ async function getProducts(cb){
 //   console.log(products);
 // });
 
-function renderListProduct(){
-  getProducts((products)=>{
-    products.forEach(renderProduct)
+function renderListProduct() {
+  getProducts((products) => {
+    products.forEach(renderProduct);
   });
 }
-function renderProduct(product){
-  let viewProduct = document.querySelector(".view-products");
-  viewProduct.insertAdjacentHTML('beforeend',
-  `
+function renderProduct(product) {
+  let viewProduct = document.querySelector('.view-products');
+  viewProduct.insertAdjacentHTML(
+    'beforeend',
+    `
   <div class="item-product swiper-slide">
   <div class="product-thumbnail">
     <img
@@ -58,46 +58,44 @@ function renderProduct(product){
     <a class="product-name" href="#"
       >${product.name}</a
     >
-    <div class="price-box">${product.price}đ</div>
+    <div class="price-box">${product.price} đ</div>
     <div class="text-details">
     ${product.description}
       <a>Xem chi tiết</a>
     </div>
   </div>
 </div>
-  `)
+  `
+  );
 }
 
-
 ///sort
-function sortAZ(){
-  getProducts((products)=>{
-    for(var i=0 ; i<products.length-1 ; i++)
-      for(var j=i+1 ; j<products.length ; j++){
-        if(products[i].name>products[j].name){
+function sortAZ() {
+  getProducts((products) => {
+    for (var i = 0; i < products.length - 1; i++)
+      for (var j = i + 1; j < products.length; j++) {
+        if (products[i].name > products[j].name) {
           var temp;
           temp = products[j];
           products[j] = products[i];
           products[i] = temp;
         }
-    }
-  }); 
+      }
+  });
 }
-function renderSortAZ(){
-  let defaultBtn = document.querySelector(".item-sort_default");
-  defaultBtn.addEventListener('click',()=>{
+function renderSortAZ() {
+  let defaultBtn = document.querySelector('.item-sort_default');
+  defaultBtn.addEventListener('click', () => {
     console.log(1);
-    
-  })
+  });
 }
 
 //start
-function start(){
+function start() {
   renderListProduct();
   renderSortAZ();
 }
 start();
-var array = ['b','c','a'];
+var array = ['b', 'c', 'a'];
 var a = array.sort();
 console.log(a);
-
