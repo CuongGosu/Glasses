a8bead1001c06d04e459147aa11d4d9c5b8e48ab
TruongHoang01 <banhathoang01@gmail.com>
2023-03-16 16:18:03 +0700

    update sort product

--- js/product.js
+++ js/product.js
@@ -69,33 +69,113 @@ function renderProduct(product) {
   );
 }
 
-///sort
-function sortAZ() {
+///sortAZ
+function renderSortAZ(a) {
+  a= [];
   getProducts((products) => {
-    for (var i = 0; i < products.length - 1; i++)
-      for (var j = i + 1; j < products.length; j++) {
-        if (products[i].name > products[j].name) {
-          var temp;
-          temp = products[j];
-          products[j] = products[i];
-          products[i] = temp;
-        }
-      }
-  });
-}
-function renderSortAZ() {
-  let defaultBtn = document.querySelector('.item-sort_default');
-  defaultBtn.addEventListener('click', () => {
-    console.log(1);
+    sortAZ(products)
+      for(var i in products)
+      a.push(products[i]);
+      let defaultBtn = document.querySelector('.item-sort_A-Z');
+      defaultBtn.addEventListener('click', () => {
+        a.forEach(renderProduct);
+      });
   });
 }
 
+function sortAZ(products){
+  for (var i = 0; i < products.length - 1; i++)
+  for (var j = i + 1; j < products.length; j++) {
+    if (products[i].name > products[j].name) {
+      var temp;
+      temp = products[j];
+      products[j] = products[i];
+      products[i] = temp;
+    }
+  }
+}
+
+//sortZA
+function sortZA(products){
+  for (var i = 0; i < products.length - 1; i++)
+  for (var j = i + 1; j < products.length; j++) {
+    if (products[i].name < products[j].name) {
+      var temp;
+      temp = products[j];
+      products[j] = products[i];
+      products[i] = temp;
+    }
+  }
+}
+
+function renderSortZA(a) {
+  a= [];
+  getProducts((products) => {
+    sortZA(products)
+      for(var i in products)
+      a.push(products[i]);
+      let defaultBtn = document.querySelector('.item-sort_Z-A');
+      defaultBtn.addEventListener('click', () => {
+        a.forEach(renderProduct);
+      });
+  });
+}
+
+//sort price ASC 
+function sortASC(products){
+  for (var i = 0; i < products.length - 1; i++)
+  for (var j = i + 1; j < products.length; j++) {
+    if (products[i].price > products[j].price) {
+      var temp;
+      temp = products[j];
+      products[j] = products[i];
+      products[i] = temp;
+    }
+  }
+}
+
+function renderSortZA(a) {
+  a= [];
+  getProducts((products) => {
+    sortASC(products)
+      for(var i in products)
+      a.push(products[i]);
+      let defaultBtn = document.querySelector('.item-sort_price-asc');
+      defaultBtn.addEventListener('click', () => {
+        a.forEach(renderProduct);
+      });
+  });
+}
+
+//sort price DESC
+function sortDESC(products){
+  for (var i = 0; i < products.length - 1; i++)
+  for (var j = i + 1; j < products.length; j++) {
+    if (products[i].price < products[j].price) {
+      var temp;
+      temp = products[j];
+      products[j] = products[i];
+      products[i] = temp;
+    }
+  }
+}
+
+function renderSortZA(a) {
+  a= [];
+  getProducts((products) => {
+    sortDESC(products)
+      for(var i in products)
+      a.push(products[i]);
+      let defaultBtn = document.querySelector('.item-sort_price-desc');
+      defaultBtn.addEventListener('click', () => {
+        a.forEach(renderProduct);
+      });
+  });
+}
 //start
 function start() {
   renderListProduct();
   renderSortAZ();
+  renderSortZA();
 }
 start();
-var array = ['b', 'c', 'a'];
-var a = array.sort();
-console.log(a);
