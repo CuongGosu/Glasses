cecc3cc3581ef4925a9dcee25aafb53f9cd68712
CuongGosu <hoangngocquoccuong@gmail.com>
2023-03-27 18:25:47 +0700

    delete signin/signup

--- js/firebase.js
+++
@@ -1,7 +0,0 @@
-  //////////////////////////////////////////////////////
-  import axios from 'axios';
-  
-  export default axios.create({
-    baseURL: "https://glasses-store-fd757-default-rtdb.asia-southeast1.firebasedatabase.app/"
-  })
-  
